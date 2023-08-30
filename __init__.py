6644b6dc2138b2907129f4a571d1e2d44640a239
Yiğit GÜMÜŞ <rawns0909@gmail.com>
2023-08-30 15:09:21 +0300

    update main __init__.py file

--- __init__.py
+++ __init__.py
@@ -17,7 +17,12 @@ __spec__ = {
     'Category': 'Personal Toolbox for multiple purposes',
     'Description': 'Toolbox for multiple purposes like ML, NN and Economics and Statistics.',
     'License': 'Apache License, Version 2.0',
-    'LicenseUrl': 'http://github.com/yigitoo/toolbox/tree/master/LICENSE.md'
+    'LicenseUrl': 'http://github.com/yigitoo/toolbox/tree/master/LICENSE.md',
+    'GithubUrl': 'http://github.com/yigitoo/pytoolbox',
+    'GithubUsername': 'yigitoo',
+    'Mail': 'rawns0909@gmail.com',
+    'Instagram': 'instagram.com/yigiittgumus',
+    'XAccount': 'x.com/yigitgumus09',
 }
 
 if __name__ == "__main__" and __package__ is None:
